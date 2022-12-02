b212d1f8e2a7ed465235f59aed30add4648d2ab0
vassdeniss <vassdeniss@gmail.com>
2022-12-03 01:13:13 +0200

    Forgot files

---
+++ EasyRank/EasyRank.Web/Models/Account/VerifyEmailViewModel.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VerifyEmailViewModel.cs" company="Denis Vasilev">
+// Copyright (c) Denis Vasilev. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyRank.Web.Models.Account
+{
+    /// <summary>
+    /// The view model for when the user is verifying their email.
+    /// </summary>
+    public class VerifyEmailViewModel
+    {
+        /// <summary>
+        /// Gets or sets the users current email.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+    }
+}
